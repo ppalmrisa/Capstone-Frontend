30f0c7355ea21fa720d5e1a56c0ae2b5febcc58c
Kornji(Blockfint) <jiradej@blockfint.com>
2024-02-17 20:57:30 +0700

    fix next-config

--- next.config.mjs
+++ next.config.mjs
@@ -3,27 +3,27 @@
  */
 
 const nextConfig = {
+  // output: 'export',
   reactStrictMode: true,
   swcMinify: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/login',
-  //       permanent: false,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false,
+      },
+    ];
+  },
   env: {
     CAPSTONE_SESSION_TOKEN: process.env.CAPSTONE_SESSION_TOKEN || 'capstone-session-token',
     BASE_URL: process.env.BASE_URL || 'http://localhost:3000',
   },
-  // output: 'export',
   // Add basePath
   // basePath: '/github-pages',
-  images: {
-    unoptimized: true,
-  },
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
