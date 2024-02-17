31d4eee2d601f01cfb01081c5b38868002ec0b24
Kornji(Blockfint) <jiradej@blockfint.com>
2024-02-17 20:05:38 +0700

    fix nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -5,19 +5,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/login',
-        permanent: false,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/login',
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
   env: {
     CAPSTONE_SESSION_TOKEN: process.env.CAPSTONE_SESSION_TOKEN || 'capstone-session-token',
     BASE_URL: process.env.BASE_URL || 'http://localhost:3000',
   },
+  output: 'export',
 };
 
 export default nextConfig;
