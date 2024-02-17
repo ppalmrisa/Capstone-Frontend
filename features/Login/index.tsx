eb7e9d1b5952917ce60f3149b36cde0ad6e6d444
Kornji(Blockfint) <jiradej@blockfint.com>
2024-02-17 18:14:17 +0700

    fix title input

--- features/Login/index.tsx
+++ features/Login/index.tsx
@@ -1,14 +1,12 @@
 'use client'
 
 import {
-    Anchor,
-    Button,
-    Checkbox,
-    Paper,
-    PasswordInput,
-    Text,
-    TextInput,
-    Title,
+  Button,
+  Checkbox,
+  Paper,
+  PasswordInput,
+  TextInput,
+  Title
 } from '@mantine/core'
 
 import classes from './styles.module.css'
@@ -18,22 +16,14 @@ export function LoginFeature() {
     <div className={classes.wrapper}>
       <Paper className={classes.form} radius={0} p={30}>
         <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
-          Welcome back to Mantine!
+          Welcome back to Capstone!
         </Title>
-
-        <TextInput label="Email address" placeholder="hello@gmail.com" size="md" />
+        <TextInput label="Email" placeholder="hello@gmail.com" size="md" />
         <PasswordInput label="Password" placeholder="Your password" mt="md" size="md" />
         <Checkbox label="Keep me logged in" mt="xl" size="md" />
         <Button fullWidth mt="xl" size="md">
           Login
         </Button>
-
-        <Text ta="center" mt="md">
-          Don&apos;t have an account?{' '}
-          <Anchor<'a'> href="#" fw={700} onClick={(event) => event.preventDefault()}>
-            Register
-          </Anchor>
-        </Text>
       </Paper>
     </div>
   );
